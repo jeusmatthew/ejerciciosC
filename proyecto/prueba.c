f9d79889bfff5d454fdf9f010e31bba106ec6331
Mateo Ortiz <a22216355@alumnos.uady.mx>
2023-03-08 22:59:31 -0600

    Eliminado funciones inecesarias y añadido otra

--- proyecto/prueba.c
+++ proyecto/prueba.c
@@ -5,23 +5,24 @@
 #define STR_SIZE 40
 
 int validarCaracter(char);
-void validadCadena(char[]);
+// void validadCadena(char[]);
 int isOperator(char);
 
 void limpiarCadena(char*);
-char* trimCadenaMemoria(char*);
+// char* trimCadenaMemoria(char*);
 
 void imprimirCaracteres(char*);
+
+void errorLexico(char*);
 
 int main(int argc, char const* argv[]) {
     char cadena[STR_SIZE] = "";
 
     gets(cadena);
     limpiarCadena(cadena);
-    imprimirCaracteres(cadena);
+    errorLexico(cadena);
+    // imprimirCaracteres(cadena);
     // puts(cadena);
-
-
 
     // char caracter = 0;
 
@@ -34,26 +35,13 @@ int main(int argc, char const* argv[]) {
     //     }
 
     // }
-
     return 0;
 }
 
 void imprimirCaracteres(char* cadena) {
     for (int i = 0; i < STR_SIZE; i++) {
-        printf("%d: %d %c\n", i, cadena[i], cadena[i]);
+        printf("%d: ascii: %d char: %c\n", i, cadena[i], cadena[i]);
     }
-}
-
-char* trimCadenaMemoria(char* cadena) {
-    char nuevaCadena[strlen(cadena)];
-    int j = 0;
-    for (int i = 0; cadena[i] != '\0'; i++) {
-        if (!isspace(cadena[i])) {
-            nuevaCadena[j++] = cadena[i];
-        }
-    }
-    nuevaCadena[j] = '\0';
-    return nuevaCadena;
 }
 
 // todavia se tienen residuos revisar
@@ -73,6 +61,18 @@ void limpiarCadena(char* cadena) {
         cadena[i] = nuevaCadena[i];
     }
 
+}
+
+void errorLexico(char* cadena) {
+    for (int i = 0; cadena[i] != '\0'; i++) {
+
+
+
+        if (validarCaracter(cadena[i]) == 1) {
+            printf("Error lexico en el caracter: %c", cadena[i]);
+            return;
+        }
+    }
 }
 
 int isOperator(char caracter) {
