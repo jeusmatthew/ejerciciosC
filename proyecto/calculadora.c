f2268192916a6bb180ab501c4a2c47bb51b98a83
Mateo Ortiz <a22216355@alumnos.uady.mx>
2023-03-11 21:21:13 -0600

    Modificaciones al manejo de errores

--- proyecto/calculadora.c
+++ proyecto/calculadora.c
@@ -10,17 +10,6 @@ Uniendo todo . . .
 
 #define STR_LEN 31
 
-//funciones
-char leerExpresion(char* Expresion); // lee expresion
-void errorSintaxis(char* Expresion); // error sintactico
-int isOperator(char caracter); //detecta si es un operador
-int isOperator2(char caracter); // detecta un operador pero sin '('
-int isOperator3(char caracter); // detecta un operador pero sin '()'
-
-void errorLexico(char*);
-int validarCaracter(char);
-
-
 //funciones de nodos
 typedef int Item;
 typedef struct nodo {
@@ -39,6 +28,14 @@ void eliminarPrimerNodo(Pila*);
 void limpiarPila(Pila*);
 int tamanioPila(Pila);
 
+//funciones
+char leerExpresion(char* Expresion); // lee expresion
+int errorSintaxis(char* Expresion); // error sintactico
+int errorLexico(char*); //Verifica si todos los caracteres son validos
+int isOperator(char caracter); //detecta si es un operador
+int isOperator2(char caracter); // detecta un operador pero sin '('
+int isOperator3(char caracter); // detecta un operador pero sin '()'
+
 void concatenarCharACadena(char, char*);
 int verificarPresedencia(char, char);
 void infijaAPosfija(char*);
@@ -51,11 +48,11 @@ int main() {
 
     puts("Ingrese una expresion aritmetica");
     leerExpresion(Expresion);
-    errorLexico(Expresion);
-    errorSintaxis(Expresion);
+
+    if (errorLexico(Expresion) || errorSintaxis(Expresion)) return 0;
+
     infijaAPosfija(Expresion);
     printf("El resultado de la expresion es: %d", obtenerResultado(Expresion));
-    
     return 0;
 }
 
@@ -66,7 +63,7 @@ char leerExpresion(char* Expresion) { //leemos expresion
     return *Expresion;
 }
 
-void errorSintaxis(char* Expresion) { // para evaluar errores
+int errorSintaxis(char* Expresion) { // para evaluar errores
     int i = 0, balance = 0, numeroEspacios;
 
     numeroEspacios = strlen(Expresion); //variable para guardar la cantidad es elementos que tiene nuestra cadena.
@@ -75,13 +72,12 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
 
         if (isOperator2(Expresion[0])) { // determina si el primer término de la expresion es un operador (error)
             printf("Error: no se permite iniciar la expresion con el operador: '%c'.\n", Expresion[0]);
-            exit(0);
+            return 1;
 
         }
         else if (isOperator3(Expresion[numeroEspacios - 1])) {  // determina si el último término de la expresion es un operador (error)
             printf("Error: no se permite finalizar la expresion con el operador: '%c'.\n", Expresion[numeroEspacios - 1]);
-            exit(0);
-
+            return 1;
         }
 
         // El balance debe estar en 0 si hay la misma cantidad de parentesis izquierdos que de derechos
@@ -89,7 +85,7 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
             balance++; //aumentar el contador para contar parentesis izquierdos
             if (Expresion[i + 1] == ')') { // si entrentra un ')' despues de un '(' es error, no pueden haber parentesis vacios
                 puts("Error: '()' no se permiten parentesis vacios.\n");
-                exit(0);
+                return 1;
             }
 
         }
@@ -98,12 +94,12 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
 
             if (Expresion[i + 1] == '(') { // si encuentra en parentesis izquierdo despues de uno derecho marca error, debe haber un operador entre un derecho y un izquierdo
                 puts("Error: se esperaba un operador en ')(' para efectuar la operacion.\n");
-                exit(0);
+                return 1;
             }
 
             if (balance < 0) { // si el balance es menor a cero, significa que hay más parentesis derechos que izquierdos, al menos uno no tuvo apertura 
                 printf("Error: cierre de parentesis sin correspondiente apertura.n");
-                exit(0);
+                return 1;
             }
 
         }
@@ -112,7 +108,7 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
             if (isOperator3(Expresion[i + 1])) { //detecta el error si despues de una apertura de parentesis hay un operador 
 
                 printf("Error: no se permite un operador despues de un parentesis izquierdo '(%c'.\n", Expresion[i + 1]);
-                exit(0);
+                return 1;
             }
 
         }
@@ -120,11 +116,11 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
             // revisa si hay dos digitos seguidos, es error ya que solo trabajamos unidades
             if (isdigit(Expresion[i + 1])) {
                 printf("Error: No se permiten dos digitos seguidos '%c%c'.\n", Expresion[i], Expresion[i + 1]);
-                exit(0);
+                return 1;
             }
             else if (Expresion[i + 1] == '(') { // si despues de un digito hay un parentesis izquierdo, indica que se esperaba un operador
                 printf("Error: se esperaba un producto en '%c(' para efectuar la operacion.\n", Expresion[i]);
-                exit(0);
+                return 1;
             }
 
         }
@@ -132,11 +128,11 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
             if (isOperator2(Expresion[i + 1])) {
                 if (Expresion[i + 1] == ')') { // si es un operador y el siguiente es parentesis, indica que se esperaba un digito
                     printf("Error: se esperaba un digito en '%c)'.\n", Expresion[i]);
-                    exit(0);
+                    return 1;
                 }
                 else {
                     printf("Error: No se permiten dos operadores seguidos '%c%c', se esperba un digito.\n", Expresion[i], Expresion[i + 1]);
-                    exit(0);
+                    return 1;
                 } // si el siguiente es un operador diferente de parentesis, indica que no pueden ir dos operadores seguidos y que se esperaba un digito
             }
         }
@@ -144,10 +140,10 @@ void errorSintaxis(char* Expresion) { // para evaluar errores
     }
     if (balance > 0) { // si el balance es menor a cero, signifa que hay más parentesis izquierdos, y al menos uno no tiene cierre correspondiente.
         printf("Error: apertura de parentesis sin correspondiente cierre.\n");
-        exit(0);
+        return 1;
     }
-    // printf("La expresion es valida.\n"); //si no cumple con ninguna de las condiciones anteriores, la expresion es valida
-
+    // printf("La expresion es valida.\n");
+    return 0; //si no cumple con ninguna de las condiciones anteriores, la expresion es valida
 }
 
 int isOperator(char caracter) {
@@ -162,22 +158,18 @@ int isOperator3(char caracter) {
     return caracter == '*' || caracter == '+' || caracter == '-' || caracter == '/' || caracter == '^';
 }
 
-
-void errorLexico(char* cadena) {
+/*
+0   Sin errores
+1   Error
+*/
+int errorLexico(char* cadena) {
     for (int i = 0; cadena[i] != '\0'; i++) {
-
-        if (validarCaracter(cadena[i]) == 1) {
+        if (!isdigit(cadena[i]) && !isOperator(cadena[i])) {
             printf("Error lexico en el caracter: %c", cadena[i]);
-            return;
+            return 1;
         }
     }
 }
-
-
-int validarCaracter(char caracter) {
-    return !isdigit(caracter) && !isOperator(caracter);
-}
-
 
 void infijaAPosfija(char* cadenaInfija) {
     Pila pila;
@@ -366,7 +358,7 @@ int obtenerResultado(char* expresion) {
             // puts("Cima 2 borrada");
 
             resultado = realizarOperacion(operando1, operando2, caracterEvaluado);
-            
+
             insertar(&pila, resultado);
         }
 
